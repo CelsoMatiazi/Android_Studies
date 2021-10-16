2e573584f1f28422f1c6dcebccc3bca24590b3dc
CelsoMatiazi <celsomatiazi@gmail.com>
2021-10-15 22:20:22 -0300

    exercicio layout animation

---
+++ app/src/main/java/com/example/cursoandroiddh/Animacoes/Exemplo1/AnimaMainActivityEx1.kt
@@ -0,0 +1,14 @@
+package com.example.cursoandroiddh.Animacoes.Exemplo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cursoandroiddh.R
+
+class AnimaMainActivityEx1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anima_main_ex1)
+
+        title = "Animações"
+    }
+}
