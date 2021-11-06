74cb1be918611617e7b7f70318f654db4115af18
CelsoMatiazi <celsomatiazi@gmail.com>
2021-11-05 21:49:03 -0300

    ajuste no constrains layout

---
+++ app/src/main/java/com/example/cursoandroiddh/Layouts/ConstraintLayout/ConstraintLayoutMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cursoandroiddh.Layouts.ConstraintLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cursoandroiddh.R
+
+class ConstraintLayoutMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_main)
+    }
+}
